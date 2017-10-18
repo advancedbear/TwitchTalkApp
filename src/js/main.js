a58a2424f9dc026e317f46c7dea054f7bf28e401
advanced_bear <advanced.bear@gmail.com>
2017-10-18 22:39:04 +0900

    動くようになった

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,71 @@
+var gui = require('nw.gui');
 var IRC = require('twitch-irc-lite');
 var say = require('say');
 var Bouyomi = require('./js/bouyomi.js');
 var conn = false;
+var client;
+var mainWindow = nw.Window.get();
+
+mainWindow.on('loaded', function(){
+    console.log(localStorage.password);
+    document.getElementById("password").value = localStorage.password;
+    document.getElementById("name").value = localStorage.name;
+    document.getElementById("channel").value = localStorage.channel;
+
+    if(localStorage.password!=null){
+        $("#loginTwitch img").attr('src', 'img/Loggedin.png');
+        $("#loginTwitch").attr('onclick', 'alert(\'You are already logged in\');');
+    }
+});
 
 function Connect(){
     let pass = document.getElementById("password").value;
     let name = document.getElementById("name").value;
     let channel = document.getElementById("channel").value;
 
-    var client = new IRC(pass, name);
-    client.join(channel);
-    client.chatEvents.addListener('message', function(channel, from, message){
-        console.log(from+': '+message);
-        say.speak(message);
-        Bouyomi.read(message);
-    });
+    localStorage.name = name;
+    localStorage.channel = channel;
+
     if(!conn){
-        document.getElementById("connButton").value = "Disconnect";
+        client = new IRC(pass, name);
+        client.chatEvents.addListener('message', function(channel, from, message){
+            console.log(from+': '+message);
+            console.log(isEnglish(message));
+            isEnglish(message) ? say.speak(message) : Bouyomi.read(message);
+        });
+        document.getElementById("connButton").innerText = "Disconnect";
         conn = true;
+        client.join(channel);
     } else {
-        document.getElementById("connButton").value = "Connect";
+        document.getElementById("connButton").innerText = "Connect";
         conn = false;
+        client.leave();
     }
 };
+
+function loginTwitch() {
+    gui.Window.open ('http://www.twitchapps.com/tmi/', {
+        width: 640,
+        height: 480
+      }, function(tmi){
+        tmi.on ('loaded', function(){
+            console.log("loaded!");
+            var tmiPage = tmi.window.document;
+            if(tmiPage.getElementById("tmiPasswordField").value != ""){
+                let Password = tmiPage.getElementById("tmiPasswordField").value;
+                document.getElementById("password").value = Password;
+                localStorage.password = Password;
+                console.log(Password);
+                tmi.close();
+            }
+        });
+    });
+};
+
+function isEnglish(message){
+    return (message.match("^(.*[｡-ﾟ０-９ａ-ｚＡ-Ｚぁ-んァ-ヶ亜-黑一-龠々ー].*)*$")) ? true : false ;
+}
+
+function replaceURL(message){
+    return message.replace('(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+');
+}
