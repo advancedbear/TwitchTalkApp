186cf008ae63f4c355892a7aa9508b52ada20b51
advanced_bear <advanced.bear@gmail.com>
2017-11-21 12:52:53 +0900

    Add description in package.json and showing in update notification.

--- src/js/updater.js
+++ src/js/updater.js
@@ -6,11 +6,14 @@ var upd = new updater(pkg);
 if(!gui.App.argv.length) {
     upd.checkNewVersion(function(error, newVersionExists, manifest) {
         if (!error && newVersionExists) {
-            alert("Update is available. Please use the latest version.\n新しいバージョンが利用可能です。最新版をご利用下さい。")
+            let mes = "Update is available. Please use the latest version.\n" 
+                + "新しいバージョンが利用可能です。最新版をご利用下さい。\n\n"
+                + "【更新内容】\n"
+                + manifest.description;
+            alert(mes)
             gui.Shell.openExternal('https://advancedbear.github.io/products.html#TwitchTalkApp');
             $('.loader').delay(1000).slideUp('slow');
         } else {
-            console.log(error);
             $('.loader').delay(1000).slideUp('slow');
         }
     });
